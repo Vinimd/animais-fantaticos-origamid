94c5475c21e32d1f1ab4461b4f247adb76cbf507
Vinicius Moda <mosaico.cc@gmail.com>
2021-12-08 15:31:13 -0300

    transformando função SoftScroll em uma classe

--- js/soft_scroll/softScroll.js
+++ js/soft_scroll/softScroll.js
@@ -1,27 +1,31 @@
-export default function sofScroll() {
-  const links = document.querySelectorAll("[data-anime='menu'] a[href^='#']");
-
-  function scrollToSection(event) {
-    event.preventDefault();
-    const id = event.target.hash;
-
-    const searchId = document.querySelector(id);
-
-    searchId.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-    // forma Alternatina
-    // const top = searchId.offsetTop;
-    // console.log(top)
-    // window.scrollTo({
-    //     top:top,
-    //     behavior:"smooth",
-    // },)
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
-  links.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  scrollToSection(event) {
+    event.preventDefault();
+    const id = event.target.hash;
+    const searchId = document.querySelector(id);
+    searchId.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.AddLinkEvent();
+    }
+    return this;
+  }
 }
