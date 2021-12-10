cb51d61bb549af0dd95ccbba6e9b94552fababa7
Vinicius Moda <mosaico.cc@gmail.com>
2021-12-10 17:27:09 -0300

    animaNumero refatorado

--- js/anima__numero/animaNumero.js
+++ js/anima__numero/animaNumero.js
@@ -1,32 +1,55 @@
-export default function initAnimaNumero() {
-  const animalsNumber = document.querySelectorAll("[data-numero]");
-  const observeTarget = document.querySelector(".numeros");
-  function animation() {
-    animalsNumber.forEach((number) => {
-      let cont = 0;
-      const spanNumber = +number.innerText;
-      const add = Math.floor(spanNumber / 100);
+export default class AnimaNumero {
+  constructor(animal, classObserver, observeTarget) {
+    this.animalsNumber = document.querySelectorAll(animal);
+    this.observerClass = classObserver;
+    this.observeTarget = document.querySelector(observeTarget);
+    this.animaNumber = this.animaNumber.bind(this);
+  }
 
-      const timer = setInterval(() => {
-        cont += add;
-        number.innerText = cont;
-        if (cont > spanNumber) {
-          number.innerText = spanNumber;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random());
+  // recebe um elemento do DOM ,contento um numero em forma de string
+  // converte a string em numero e começa incrmentar a partir de 0
+  static numberIncrement(number) {
+    let cont = 0;
+    const spanNumber = +number.innerText;
+    const add = Math.floor(spanNumber / 100);
+
+    const timer = setInterval(() => {
+      cont += add;
+      number.innerText = cont;
+      if (cont > spanNumber) {
+        number.innerText = spanNumber;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  // adicona o efeito de animação aos numeros selecionados
+  animation() {
+    this.animalsNumber.forEach((number) => {
+      this.constructor.numberIncrement(number);
     });
   }
-  let observer;
-  function animaNumber(mutation) {
-    if (mutation[0].target.classList.contains("scroll-change")) {
-      observer.disconnect();
-      animation();
+
+  animaNumber(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animation();
     }
   }
-  observer = new MutationObserver(animaNumber);
 
-  observer.observe(observeTarget, {
-    attributes: true,
-  });
+  // adicona observerMutation para verificar quando a classe scroll-change é adicionda
+  // ao elemento target
+  addObserverMutation() {
+    this.observer = new MutationObserver(this.animaNumber);
+    this.observer.observe(this.observeTarget, {
+      attributes: true,
+    });
+  }
+
+  init() {
+    if (this.animalsNumber.length && this.observeTarget && this.observerClass) {
+      this.addObserverMutation();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ import Accordion from "./accordion__list/accordionList.js";
 import NavTab from "./nav__tab/navTab.js";
 import Modal from "./modal/modal.js";
 import Tooltip from "./tooltip/tooltip.js";
+import InitFetchAnimais from "./fetch-animais/fetch-animais.js";
 import dataRight from "./add_show_right/addShowRight.js";
 import changeScrollanimation from "./animacao__ao__scroll/animacaoScroll.js";
 import menuDropDown from "./drop_down/menuDropDown.js";
 import mobileMenu from "./mobile__menu/menuMobile.js";
 import initOpen from "./open/open.js";
-import InitFetchAnimais from "./fetch-animais/fetch-animais.js";
 
 window.onload = function init() {
   const scrollSuave = new ScrollSuave("[data-anime='menu'] a[href^='#']");
